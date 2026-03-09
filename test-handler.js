5c99cac7d85e125f10ae075e33332516a5de9373
David Haughton <dhaughton@bluewin.ch>
2026-03-09 07:57:13 +0100

    Phase3: Complete

---
+++ test-handler.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import { handler } from "./lambda/handler.js";
+
+async function run() {
+
+  const event = JSON.parse(
+    fs.readFileSync("./test-event.json", "utf8")
+  );
+
+  const result = await handler(event);
+
+  console.log("\nLambda response:\n");
+  console.log(JSON.stringify(result, null, 2));
+
+}
+
+run();
