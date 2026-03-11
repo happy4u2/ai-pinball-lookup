a9c5b8b415bd5d904a76ad92369329e660e4a10e
David Haughton <dhaughton@bluewin.ch>
2026-03-11 10:47:09 +0100

    Dynamoclient.js update

--- lambda/scripts/dynamoClient.js
+++ lambda/scripts/dynamoClient.js
@@ -1,8 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || "eu-central-1",
-});
+const client = new DynamoDBClient({});
 
-export const docClient = DynamoDBDocumentClient.from(client);
+export const dynamoDocClient = DynamoDBDocumentClient.from(client);
